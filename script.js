c153f288d84efa45600fa8ee867f4423df013a8d
JavidGasim <gasimovcavid777@gmail.com>
2024-06-19 18:27:52 +0400

    change 1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function ScrollUp() {
-  document.getElementById("scrollToUp").addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("scrollToUp").addEventListener("click", function() {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+      });
   });
-}
+});
